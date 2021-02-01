1f955d666d1181c2f934036c70e428d1e3b506bb
hellokena <42730820+hellokena@users.noreply.github.com>
2021-02-02 02:44:13 +0900

    2021-02-02

---
+++ 코딩테스트 연습/정렬/PGR#Lv1_K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    for c in commands:
+        new_array = []
+        for i in range(c[0]-1, c[1]):
+            new_array.append(array[i])
+        new_array = sorted(new_array)
+        temp = new_array[c[2]-1]
+        answer.append(temp)
+    return answer
