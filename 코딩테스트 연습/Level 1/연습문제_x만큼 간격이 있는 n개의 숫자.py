18f132a9613b6a841553ba8098fc4b02085d9b82
hellokena <42730820+hellokena@users.noreply.github.com>
2021-02-03 23:28:40 +0900

    2021-02-03

--- 코딩테스트 연습/Level 1/연습문제_x만큼 간격이 있는 n개의 숫자.py
+++ 코딩테스트 연습/Level 1/연습문제_x만큼 간격이 있는 n개의 숫자.py
@@ -1,9 +1,5 @@
 def solution(x, n):
     answer = []
-    new_x=abs(x)
-    for i in range(new_x, new_x*n+1, new_x):
-        if x>=0:
-            answer.append(i)
-        else:
-            answer.append(-i)
+    for i in range(1, n+1):
+        answer.append(x*i)
     return answer
